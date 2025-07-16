c6ee266d9418e2b2d5975fd166bd1f6ee707bf94
dhruv7225 <dhruvp7225@gmail.com>
2025-07-16 22:19:16 +0530

    Update toggle-container.tsx

--- src/containers/toggle-container.tsx
+++ src/containers/toggle-container.tsx
@@ -3,7 +3,6 @@ import { Menu } from "lucide-react";
 import { useAuth } from "@clerk/clerk-react";
 
 import { cn } from "@/lib/utils";
-
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 export const ToggleContainer = () => {
@@ -17,58 +16,31 @@ export const ToggleContainer = () => {
       <SheetContent>
         <nav>
           <ul className="flex flex-col items-start gap-8">
+            <NavLink
+              className={({ isActive }) =>
+                cn(
+                  "text-base text-neutral-600",
+                  isActive && "text-neutral-900 font-semibold"
+                )
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
 
             <NavLink
-                className={({ isActive }) =>
-                  cn(
-                    "text-base text-neutral-600",
-                    isActive && "text-neutral-900 font-semibold"
-                  )
-                }
-                to={"/"}
-              >
-                Home
-              </NavLink>
-              <NavLink
-                className={({ isActive }) =>
-                  cn(
-                    "text-base text-neutral-600",
-                    isActive && "text-neutral-900 font-semibold"
-                  )
-                }
-                to={"/services"}
-              >
-                Services
-              </NavLink>
-              {userId && (
-                <NavLink
-                  className={({ isActive }) =>
-                    cn(
-                      "text-base text-neutral-600",
-                      isActive && "text-neutral-900 font-semibold"
-                    )
-                  }
-                  to={"/prepare"}
-                >
-                  Prepare
-                </NavLink>
-              )
-
+              className={({ isActive }) =>
+                cn(
+                  "text-base text-neutral-600",
+                  isActive && "text-neutral-900 font-semibold"
+                )
               }
+              to="/services"
+            >
+              Services
+            </NavLink>
 
-              {userId && (
-                <NavLink
-                  className={({ isActive }) =>
-                    cn(
-                      "text-base text-neutral-600",
-                      isActive && "text-neutral-900 font-semibold"
-                    )
-                  }
-                  to={"/generate"}
-                >
-                  Take an Interview
-                </NavLink>
-              )}
+            {userId && (
               <NavLink
                 className={({ isActive }) =>
                   cn(
@@ -76,22 +48,49 @@ export const ToggleContainer = () => {
                     isActive && "text-neutral-900 font-semibold"
                   )
                 }
-                to={"/about"}
+                to="/prepare"
               >
-                About Us
-              </NavLink>
-              <NavLink
-                className={({ isActive }) =>
-                  cn(
-                    "text-base text-neutral-600",
-                    isActive && "text-neutral-900 font-semibold"
-                  )
-                }
-                to={"/contact"}
-              >
-                Contact Us
+                Prepare
               </NavLink>
             )}
+
+            {userId && (
+              <NavLink
+                className={({ isActive }) =>
+                  cn(
+                    "text-base text-neutral-600",
+                    isActive && "text-neutral-900 font-semibold"
+                  )
+                }
+                to="/generate"
+              >
+                Take an Interview
+              </NavLink>
+            )}
+
+            <NavLink
+              className={({ isActive }) =>
+                cn(
+                  "text-base text-neutral-600",
+                  isActive && "text-neutral-900 font-semibold"
+                )
+              }
+              to="/about"
+            >
+              About Us
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) =>
+                cn(
+                  "text-base text-neutral-600",
+                  isActive && "text-neutral-900 font-semibold"
+                )
+              }
+              to="/contact"
+            >
+              Contact Us
+            </NavLink>
           </ul>
         </nav>
       </SheetContent>
